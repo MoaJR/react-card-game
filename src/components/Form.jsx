c9249eb78cecf13004c46e601b36ff74f07c6ed3
Moa Junior <moacirfonsecajr@gmail.com>
2022-10-26 15:31:46 -0300

    Adiciona props ao form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,41 +2,101 @@ import React from 'react';
 
 import '../style/Form.scss';
 
-function Form() {
+function Form({
+  cardName,
+  cardDescription,
+  cardAttr1,
+  cardAttr2,
+  cardAttr3,
+  cardImage,
+  cardRare,
+  cardTrunfo,
+  hasTrunfo,
+  isSaveButtonDisabled,
+  onInputChange,
+  onSaveButtonClick,
+}) {
   const onSubmit = (e) => {
     e.preventDefault();
   };
 
   return (
     <div className="container">
-      <form method="get" className="Form" onSubmit={ onSubmit }>
+      <form
+        method="get"
+        className="Form"
+        onSubmit={ onSubmit }
+      >
         <label htmlFor="name">
           Nome
-          <input data-testid="name-input" type="text" name="name" />
+          <input
+            data-testid="name-input"
+            type="text"
+            name="name"
+            value={ cardName }
+            onChange={ onInputChange }
+          />
         </label>
         <label htmlFor="description">
           Descrição
-          <textarea name="description" data-testid="description-input" />
+          <textarea
+            name="description"
+            data-testid="description-input"
+            value={ cardDescription }
+            onChange={ onInputChange }
+          />
         </label>
         <label htmlFor="attr1">
           Atributo 1
-          <input type="number" name="attr1" min={ 0 } data-testid="attr1-input" />
+          <input
+            type="number"
+            name="attr1"
+            min={ 0 }
+            data-testid="attr1-input"
+            value={ cardAttr1 }
+            onChange={ onInputChange }
+          />
         </label>
         <label htmlFor="attr2">
           Atributo 2
-          <input type="number" name="attr2" min={ 0 } data-testid="attr2-input" />
+          <input
+            type="number"
+            name="attr2"
+            min={ 0 }
+            data-testid="attr2-input"
+            value={ cardAttr2 }
+            onChange={ onInputChange }
+          />
         </label>
         <label htmlFor="attr3">
           Atributo 3
-          <input type="number" name="attr3" min={ 0 } data-testid="attr3-input" />
+          <input
+            type="number"
+            name="attr3"
+            min={ 0 }
+            data-testid="attr3-input"
+            value={ cardAttr3 }
+            onChange={ onInputChange }
+          />
         </label>
         <label htmlFor="image">
           Imagem
-          <input type="text" name="image" data-testid="image-input" />
+          <input
+            type="text"
+            name="image"
+            data-testid="image-input"
+            value={ cardImage }
+            onChange={ onInputChange }
+          />
         </label>
-        <label htmlFor="rarity">
+        <label htmlFor="rare">
           Raridade
-          <select name="rarity" data-testid="rare-input">
+          <select
+            name="rare"
+            data-testid="rare-input"
+            value={ cardRare }
+            onChange={ onInputChange }
+          >
             <option value="normal">Normal</option>
             <option value="raro">Raro</option>
             <option value="muito raro">Muito Raro</option>
@@ -44,9 +104,22 @@ function Form() {
         </label>
         <label htmlFor="superTrunfo">
           Super Trunfo
-          <input type="checkbox" name="superTrunfo" data-testid="trunfo-input" />
+          <input
+            type="checkbox"
+            name="superTrunfo"
+            data-testid="trunfo-input"
+            checked={ cardTrunfo }
+            onChange={ onInputChange }
+          />
         </label>
-        <button type="submit" data-testid="save-button">Salvar</button>
+        <button
+          type="submit"
+          data-testid="save-button"
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
+        >
+          Salvar
+        </button>
       </form>
     </div>
   );
